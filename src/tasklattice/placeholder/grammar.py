fe913be16498de2bcc5b24d4e3e345252d20e447
Zachary Meadows <zmeadows@gmail.com>
2025-08-08 18:08:08 -0400

    added grammar.py and associated lark grammar loading test

---
+++ src/tasklattice/placeholder/grammar.py
@@ -0,0 +1,39 @@
+TL_GRAMMAR = r"""
+start: "TL" identifier "=" literal ("," pair)*
+
+pair: identifier ":" meta_value
+
+identifier: CNAME
+
+number: SIGNED_NUMBER
+
+string: ESCAPED_STRING
+
+?literal: string
+        | number
+        | boolean
+
+boolean: "true"                -> true
+       | "false"               -> false
+
+?meta_value: literal
+           | interval
+           | set
+           | identifier
+
+interval: lpar number "," number rpar
+
+lpar: "(" -> lopen
+    | "[" -> lclosed
+
+rpar: ")" -> ropen
+    | "]" -> rclosed
+
+set: "{" [literal ("," literal)*] "}"
+
+%import common.CNAME
+%import common.SIGNED_NUMBER
+%import common.ESCAPED_STRING
+%import common.WS
+%ignore WS
+"""
---
+++ tests/placeholder/test_grammar.py
@@ -0,0 +1,9 @@
+from tasklattice.placeholder.grammar import TL_GRAMMAR
+
+from lark.load_grammar import load_grammar
+
+def test_grammar_loads() -> None:
+    load_grammar(TL_GRAMMAR, None, (), False)
+
+
+
