22315a0fdc4888a722b347053d6c659ecb4479db
Zachary Meadows <zmeadows@gmail.com>
2025-08-08 18:08:08 -0400

    added grammar.py and associated lark grammar loading test

--- tests/placeholder/test_grammar.py
+++ tests/placeholder/test_grammar.py
@@ -3,7 +3,11 @@ from tasklattice.placeholder.grammar import TL_GRAMMAR
 from lark.load_grammar import load_grammar
 
 def test_grammar_loads() -> None:
+<<<<<<< HEAD
     load_grammar(TL_GRAMMAR, None, (), False)
+=======
+    load_grammar(TL_GRAMMAR, None, (), False) # type: ignore
+>>>>>>> 3e29e27 (added grammar.py and associated lark grammar loading test)
 
 
 
